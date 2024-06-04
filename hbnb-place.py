a468835760e60dc3feb070dafdc34692cea91674
Ho2bes <8696@holbertonstudents.com>
2024-06-04 16:47:01 +0200

    add class place and def code

---
+++ hbnb-place.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"class and methods of place for HBnB"
+
+
+class Place:
+    """class place with his methods"""
+    def add_review(self, review):
+        pass  # Mettez ici le code pour ajouter un commentaire
+
+    def calculate_total_price(self):
+        pass  # Mettez ici le code pour calculer le prix total
+
+    def list_amenities(self):
+        pass  # Mettez ici le code pour lister les équipements
+
+    def check_availability(self):
+        pass  # Mettez ici le code pour vérifier la disponibilité
+
+    def list_reviews(self):
+        pass  # Mettez ici le code pour lister les commentaires
+
+    def set_number_guests(self, number):
+        self.number_guests = number
+
+    def add_description(self, description):
+        pass  # Mettez ici le code pour ajouter une description
+
+    def set_number_rooms(self, number):
+        self.number_rooms = number
+
+    def set_location(self, location):
+        self.location = location
