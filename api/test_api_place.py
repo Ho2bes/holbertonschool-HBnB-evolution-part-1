6ef415cbe61c0eb1135b71a10df05e462bc47d5f
Ho2bes <8696@holbertonstudents.com>
2024-06-05 16:25:14 +0200

    change the test of api place

--- api/test_api_place.py
+++ api/test_api_place.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+"""unnistest"""
+
+
 import unittest
 from flask import Flask, jsonify
-from unittest.mock import MagicMock
+from unittest.mock import patch
 from api.place_routes import app
 
 class TestPlaceRoutes(unittest.TestCase):
@@ -8,20 +12,23 @@ class TestPlaceRoutes(unittest.TestCase):
         # Création d'une application Flask test
         self.app = app.test_client()
 
-        # Mock de PlaceRepository pour simuler la persistance des données
-        self.mock_repo = MagicMock()
-        app.place_repo = self.mock_repo
+    @patch('api.place_routes.place_repo')  # Mock de PlaceRepository
+    def test_get_place_existing(self, mock_repo):
+        # Données de test
+        place_data = {'location': 'Paris', 'number_guests': 4, 'number_rooms': 2}
+        mock_repo.get_place.return_value = place_data
 
-    def test_get_place_existing(self):
         # Test de la route pour récupérer un lieu existant
-        self.mock_repo.get_place.return_value = {'location': 'Paris', 'number_guests': 4, 'number_rooms': 2}
         response = self.app.get('/places/123')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {'location': 'Paris', 'number_guests': 4, 'number_rooms': 2})
+        self.assertEqual(response.json, place_data)
 
-    def test_get_place_nonexistent(self):
+    @patch('api.place_routes.place_repo')  # Mock de PlaceRepository
+    def test_get_place_nonexistent(self, mock_repo):
+        # Mock de la méthode get_place pour retourner None (lieu inexistant)
+        mock_repo.get_place.return_value = None
+
         # Test de la route pour récupérer un lieu inexistant
-        self.mock_repo.get_place.return_value = None
         response = self.app.get('/places/456')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json, {'error': 'Place not found'})
