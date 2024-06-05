2275d16823f1d86be234f252c454dacd99b78598
Ho2bes <8696@holbertonstudents.com>
2024-06-05 09:07:15 +0200

    add method in class place

--- hbnb-place.py
+++ hbnb-place.py
@@ -4,29 +4,50 @@
 
 class Place:
     """class place with his methods"""
-    def add_review(self, review):
-        pass  # Mettez ici le code pour ajouter un commentaire
+    def __init__(self, location, number_guests, number_rooms):
+        self.location = location
+        self.number_guests = number_guests
+        self.number_rooms = number_rooms
+        self.reviews = []
+        self.amenities = []
+        self.description = ""
+        self.availability = True
 
-    def calculate_total_price(self):
-        pass  # Mettez ici le code pour calculer le prix total
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def calculate_total_price(self, nightly_rate):
+        return nightly_rate * self.number_guests  # Supposons que le prix soit calculé par nuit
 
     def list_amenities(self):
-        pass  # Mettez ici le code pour lister les équipements
+        return self.amenities
 
     def check_availability(self):
-        pass  # Mettez ici le code pour vérifier la disponibilité
+        return self.availability
 
     def list_reviews(self):
-        pass  # Mettez ici le code pour lister les commentaires
+        return self.reviews
 
     def set_number_guests(self, number):
         self.number_guests = number
 
     def add_description(self, description):
-        pass  # Mettez ici le code pour ajouter une description
+        self.description = description
 
     def set_number_rooms(self, number):
         self.number_rooms = number
 
     def set_location(self, location):
         self.location = location
+
+    def add_amenity(self, amenity):
+        self.amenities.append(amenity)
+
+    def toggle_availability(self):
+        self.availability = not self.availability
+
+    def get_description(self):
+        return self.description
+
+    def get_location(self):
+        return self.location
