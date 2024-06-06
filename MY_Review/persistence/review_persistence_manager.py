bed36bbb0f63b6456874707cb896b5e579318d58
Francianeny <nirinasoabozy@yahoo.fr>
2024-06-07 01:32:03 +0200

    review_persistence_manager.py

---
+++ MY_Review/persistence/review_persistence_manager.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from .persistence_manager import IPersistenceManager
+
+class ReviewPersistenceManager(IPersistenceManager):
+    def __init__(self):
+        self.reviews = {}
+
+    def save(self, review):
+        self.reviews[review.review_id] = review
+
+    def retrieve(self, review_id):
+        return self.reviews.get(review_id, None)
+
+    def update(self, review):
+        if review.review_id in self.reviews:
+            self.reviews[review.review_id] = review
+
+    def delete(self, review_id):
+        if review_id in self.reviews:
+            del self.reviews[review_id]
