942cf4ae58777132a703ce929280ee2dfafd6e90
Francianeny <nirinasoabozy@yahoo.fr>
2024-06-11 12:02:24 +0200

    remove docker for test

--- place_7/main.py
+++ place_7/main.py
@@ -24,10 +24,10 @@ api.add_namespace(city_api, path='/cities')
 #modication for docker
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(debug=True)
 
 #code without docker
-#if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
