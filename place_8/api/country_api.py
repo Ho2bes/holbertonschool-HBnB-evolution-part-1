428b7da1bc40af1ec5e323c1501b60c3804094eb
Ho2bes <8696@holbertonstudents.com>
2024-06-12 11:22:44 +0200

    update place_8 by fixing country with all get delete udptate...

--- place_8/api/country_api.py
+++ place_8/api/country_api.py
@@ -1,32 +1,68 @@
 #!/usr/bin/python3
-
-
 # API for managing countries
-from flask_restx import Namespace, Resource
+
+from flask import request
+from flask_restx import Namespace, Resource, fields
 from data_manager import DataManager
 
-api = Namespace('countries', description='Countries related operations')
+api = Namespace('countries', description='Country related operations')
+
 data_manager = DataManager()
 
-# Route for managing countries
+# Data model for creating a country
+country_model = api.model('Country', {
+    'name': fields.String(required=True, description='Country name')
+})
+
 @api.route('/')
 class Countries(Resource):
+    @api.marshal_list_with(country_model)
     def get(self):
-        """Fetches all countries."""
-        return data_manager.get_all_countries()
+        """Retrieve all countries."""
+        all_countries = data_manager.get_all_countries()
+        return all_countries
 
-@api.route('/<string:country_code>')
+    @api.expect(country_model)
+    @api.response(201, 'Country successfully created')
+    @api.response(400, 'Invalid request')
+    def post(self):
+        """Create a new country."""
+        new_country_data = request.json
+        country_id = data_manager.save_country(new_country_data)
+        response_message = {'message': 'Country successfully created', 'country_id': country_id}
+        return response_message, 201
+
+@api.route('/<int:country_id>')
 class CountryResource(Resource):
-    def get(self, country_code):
-        """Fetches a country by its code."""
-        country = data_manager.get_country(country_code)
-        if country:
-            return country
+    @api.marshal_with(country_model)
+    @api.response(404, 'Country not found')
+    def get(self, country_id):
+        """Retrieve a country by its ID."""
+        country_data = data_manager.get_country(country_id)
+        if country_data:
+            return country_data
         else:
             api.abort(404, "Country not found")
 
-@api.route('/<string:country_code>/cities')
-class CountryCities(Resource):
-    def get(self, country_code):
-        """Fetches all cities belonging to a specific country."""
-        return data_manager.get_cities_by_country(country_code)
+    @api.expect(country_model)
+    @api.response(204, 'Country successfully updated')
+    @api.response(400, 'Invalid request')
+    @api.response(404, 'Country not found')
+    def put(self, country_id):
+        """Update an existing country."""
+        new_country_data = request.json
+        updated = data_manager.update_country(country_id, new_country_data)
+        if updated:
+            return '', 204
+        else:
+            api.abort(404, "Country not found")
+
+    @api.response(204, 'Country successfully deleted')
+    @api.response(404, 'Country not found')
+    def delete(self, country_id):
+        """Delete an existing country."""
+        deleted = data_manager.delete_country(country_id)
+        if deleted:
+            return '', 204
+        else:
+            api.abort(404, "Country not found")
